bfc21591554b10be5de78fad8ea676c5bccdb1c1
George Rapeanu <alexandrurapeanu@yahoo.com>
2023-04-03 16:59:20 +0300

    removes angular run and install from aws

--- install_script.sh
+++ install_script.sh
@@ -1,13 +1,8 @@
 sudo apt-get update;
 sudo apt-get install -y openjdk-19-jdk-headless;
-curl -fsSL https://deb.nodesource.com/setup_18.x | bash - &&\
-apt-get install -y nodejs
-npm install npm@latest -g
-npm install npm@latest -g
 docker create  \
   --name sdi-postgres \
   -e POSTGRES_PASSWORD=sdi-postgres \
   -p 5432:5432 \
   -v sdi-volume:/var/lib/postgresql/data \
   sdi-postgres;
-npm install http-server -g;
--- run_script.sh
+++ run_script.sh
@@ -2,28 +2,12 @@ docker stop sdi-postgres;
 cd /home/ubuntu/lab-5x-georgerapeanu/;
 git pull
 
-cd frontend;
-npm install;
-IP=$(curl -s http://169.254.169.254/latest/meta-data/public-ipv4);
-API_URL="http://$IP:8080";
-
-echo "export const environment = {
-    apiURL: '$API_URL'
-};
-" > ./src/environments/environment.production.ts
-ng build --configuration=production;
-
 docker start sdi-postgres;
-cd ../;
+sleep 5;
+
 cd backend
 ./mvnw clean package;
-cd ..;
 
-sleep 5;
-cd backend;
 java -jar ./target/*.jar &
-sleep 5;
-cd ../frontend;
-cd dist/frontend;
-sudo http-server -p 80 -o &
+
 
