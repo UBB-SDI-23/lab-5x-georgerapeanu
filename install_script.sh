615a1e9d3e3f89ab6c7c12be8b8266706abea764
George Rapeanu <alexandrurapeanu@yahoo.com>
2023-04-03 14:50:42 +0300

    install_script update

--- install_script.sh
+++ install_script.sh
@@ -15,6 +15,6 @@ rm -rf /usr/local/go;
 tar -C /usr/local -xzf go1.20.2.linux-amd64.tar.gz;
 rm go1.20.2.linux-amd64.tar.gz;
 echo 'export PATH=$PATH:/usr/local/go/bin' >> ~/.profile;
-/usr/local/go/bin install github.com/hairyhenderson/gomplate/v4/cmd/gomplate@latest;
+sudo /usr/local/go/bin install github.com/hairyhenderson/gomplate/v4/cmd/gomplate@latest;
 npm install http-server -g;
 echo 'Logout to complete installation';
