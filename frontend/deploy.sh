46ee08e2cb22bd751802d866446ec846e1606e6c
George Rapeanu <alexandrurapeanu@yahoo.com>
2023-04-10 16:20:01 +0300

    improves deploy script

---
+++ frontend/deploy.sh
@@ -0,0 +1,11 @@
+API_URL=$MANUAL_API_URL;
+
+if [ ! -z $INCOMING_HOOK_BODY ] ; then
+    API_URL=$INCOMING_HOOK_BODY;
+fi;
+
+for i in ./src/environments/*.tmpl;do 
+    cat $i | envsubst > ${i%.*};
+done; 
+
+ng build --configuration=production;
