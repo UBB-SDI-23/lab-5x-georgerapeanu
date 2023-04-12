ab8c6dbf316f8ece0e6f6295b877babb44f5fcd0
George Rapeanu <alexandrurapeanu@yahoo.com>
2023-04-12 06:40:12 +0300

    remove docker

--- backend/run_script.sh
+++ backend/run_script.sh
@@ -7,12 +7,8 @@ if [ -z $WEBHOOK_URL ] ; then
   exit 1;
 fi;
 
-docker stop sdi-postgres;
 cd /home/ubuntu/lab-5x-georgerapeanu/;
 git pull
-
-docker start sdi-postgres;
-sleep 5;
 
 cd backend
 ./mvnw clean package;
