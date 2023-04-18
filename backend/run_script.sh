f335fde82b02353498408753b49a1b95917cae23
George Rapeanu <alexandrurapeanu@yahoo.com>
2023-04-18 22:15:43 +0300

    changes run script

--- backend/run_script.sh
+++ backend/run_script.sh
@@ -17,6 +17,6 @@ RELEASE_URL=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/UBB-S
 PACKAGE_URL=$(echo $RELEASE_URL | sed -E "s/(\/tag)(\/[^/]*)$/\/download\2/")
 curl -sL --output ./target/sdi.war $PACKAGE_URL/sdi.war
 
-docker-compose up;
+docker-compose up&
 
 curl -X POST -d "http://$(curl http://169.254.169.254/latest/meta-data/public-ipv4):8080" $WEBHOOK_URL
