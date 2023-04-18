11c284086945363bb10c7bb5a96f293a9c6ad88e
George Rapeanu <alexandrurapeanu@yahoo.com>
2023-04-18 19:04:03 +0300

    Adds docker compose to make things easier(only dev right now)

--- backend/src/main/java/com/example/app/MainApplication.java
+++ backend/src/main/java/com/example/app/MainApplication.java
@@ -2,10 +2,16 @@ package com.example.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MainApplication {
+public class MainApplication extends SpringBootServletInitializer {
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MainApplication .class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
     }
