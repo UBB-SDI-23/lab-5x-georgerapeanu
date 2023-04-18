2e946b67ce15279e99341fe54e305bd964dc5bc5
George Rapeanu <alexandrurapeanu@yahoo.com>
2023-04-18 22:02:24 +0300

    adds docker compose for production

--- backend/run_script.sh
+++ backend/run_script.sh
@@ -7,11 +7,16 @@ if [ -z $WEBHOOK_URL ] ; then
   exit 1;
 fi;
 
-cd /home/ubuntu/lab-5x-georgerapeanu/;
+cd /home/ubuntu/lab-5x-georgerapeanu/
 git pull
 
 cd backend
-./mvnw clean package -Dspring.profiles.active=prod;
-java -jar ./target/*.jar --spring.profiles.active=prod &
+mkdir target
+
+RELEASE_URL=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/UBB-SDI-23/lab-5x-georgerapeanu/releases/latest)
+PACKAGE_URL=$(echo $RELEASE_URL | sed -E "s/(\/tag)(\/[^/]*)$/\/download\2/")
+curl -sL --output ./target/sdi.war $PACKAGE_URL/sdi.war
+
+docker-compose up;
 
 curl -X POST -d "http://$(curl http://169.254.169.254/latest/meta-data/public-ipv4):8080" $WEBHOOK_URL
