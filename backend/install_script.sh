8e47142973032068282f05084bb8d0fab6b68e11
George Rapeanu <alexandrurapeanu@yahoo.com>
2023-04-18 22:06:59 +0300

    modifies install script

--- backend/install_script.sh
+++ backend/install_script.sh
@@ -1,12 +1,11 @@
 sudo apt-get update
 sudo apt-get install -y openjdk-19-jdk-headless
 sudo apt-get install -y psql postgresql-client postgresql-client-common
-sudo apt install apt-transport-https ca-certificates curl software-properties-common
+sudo apt install -y apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
 apt-cache policy docker-ce
-sudo apt install docker-ce
+sudo apt install -y docker-ce
 sudo usermod -aG docker ${USER}
-su - ${USER}
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
