bc9bc04219fc70ecd9f5321529fe6efc5e18b354
George Rapeanu <alexandrurapeanu@yahoo.com>
2023-04-19 14:19:15 +0300

    modifies scripts

--- backend/install_script.sh
+++ backend/install_script.sh
@@ -12,4 +12,4 @@ sudo chmod +x /usr/local/bin/docker-compose
 sudo snap install core; sudo snap refresh core
 sudo snap install --classic certbot
 sudo ln -s /snap/bin/certbot /usr/bin/certbot
-sudo certbot certonly --nginx
+sudo certbot certonly
