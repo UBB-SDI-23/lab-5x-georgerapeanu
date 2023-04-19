e5a034a783a37aecd1c34d28f74952b90cb7b03b
George Rapeanu <alexandrurapeanu@yahoo.com>
2023-04-19 14:54:50 +0300

    removes docker run as root

--- backend/run_script.sh
+++ backend/run_script.sh
@@ -17,6 +17,6 @@ RELEASE_URL=$(curl -Ls -o /dev/null -w %{url_effective} https://github.com/UBB-S
 PACKAGE_URL=$(echo $RELEASE_URL | sed -E "s/(\/tag)(\/[^/]*)$/\/download\2/")
 curl -sL --output ./target/sdi.war $PACKAGE_URL/sdi.war
 
-sudo docker-compose up&
+docker-compose up&
 
 #curl -X POST -d "https://$(curl -s http://metadata.google.internal/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip -H "Metadata-Flavor: Google"):443" $WEBHOOK_URL
