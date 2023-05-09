a39d8d0e902bec6b5927632114c49b5bf61ee61d
George Rapeanu <alexandrurapeanu@yahoo.com>
2023-05-09 16:39:11 +0300

    removes buttons when unauthorized

--- frontend/src/app/components/abstract/abstract-overview-page/abstract-overview-page.component.ts
+++ frontend/src/app/components/abstract/abstract-overview-page/abstract-overview-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, Input } from '@angular/core';
+import { LoginService } from 'src/app/services/login.service';
+import { UserService } from 'src/app/services/user-service';
+import { Role } from 'src/app/model/Role';
 
 @Component({
   selector: 'app-abstract-overview-page',
@@ -10,4 +13,19 @@ export class AbstractOverviewPageComponent<TData> {
   @Input() currentPage: number = 0;
   @Input() currentSize: number = 10;
   @Input() actionable: boolean = false;
+  protected role: Role | null = null;
+
+
+  constructor(
+    private loginService: LoginService,
+    private userService: UserService
+  ) {
+    loginService.getUserHandleObservable().subscribe({
+      next: (handle) => {
+        userService.getUserRole(handle || "visitor").subscribe({
+          next: (role) => {this.role = role;}
+        })
+      }
+    })
+  }
 }
